e7105e0d54a08b737002a6ec5233e0a639f7f036
souheil220 <souhil433@gmail.com>
2024-09-03 18:50:32 +0100

    implemented the DockerFile

--- frontend/src/Provider/ApolloProvider.tsx
+++ frontend/src/Provider/ApolloProvider.tsx
@@ -1,9 +1,18 @@
 // src/ApolloProvider.tsx
-import React, { ReactNode } from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider as Provider } from '@apollo/client';
+import React, { ReactNode } from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider as Provider,
+} from "@apollo/client";
 
+const BACK_PORT = process.env.REACT_APP_BACK_PORT;
+
+if (!BACK_PORT) {
+  throw new Error("BACK_PORT environment variable is not defined");
+}
 const client = new ApolloClient({
-  uri: 'http://localhost:4000', // L'URL de votre serveur Apollo GraphQL
+  uri: `http://localhost:${BACK_PORT}`, // L'URL de votre serveur Apollo GraphQL
   cache: new InMemoryCache(),
 });
 
